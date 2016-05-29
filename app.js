da232addaab841be90da7e8b09a7ba6182f63110
Danielle H <daheber@gmail.com>
2016-05-29 12:20:41 -0700

    printForecast

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 //Problem: we need a way to get weather forecast info based off of a zipcode
 //Solution: Use nodejs to connect to the Weather Underground API to retrieve and print out weather forecast information
 
+var location = 97209;
+
 //Print out message
+function printForecast(weather, temperature){
+  var message = 'The weather is currently ' + weather + ' with a temperatue of ' + temperature + '.';
+}
 
 //Print out error message
+function printError(error){
+  console.error(error.message);
+}
 
 //Connect to the API
+var getForecast = function(location){
   //Read the data
   //Parse the data
   //Print the data out
 
-//Error handling
+  //Error handling
+};
+
+getForecast(location);
