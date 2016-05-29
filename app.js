42ee3738df951513537ba3a1009bac90c6c03b68
Danielle H <daheber@gmail.com>
2016-05-29 11:59:16 -0700

    plan of action

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('hello world');
+//Problem: we need a way to get weather forecast info based off of a zipcode
+//Solution: Use nodejs to connect to the Weather Underground API to retrieve and print out weather forecast information
+
+//Print out message
+
+//Print out error message
+
+//Connect to the API
+  //Read the data
+  //Parse the data
+  //Print the data out
+
+//Error handling
